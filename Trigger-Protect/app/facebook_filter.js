045e5243903b1fc8c3ad355ccc083ac8f26374be
Ananya W Cleetus <ananyacleetus@gmail.com>
2016-07-26 14:16:46 -0700

    Testing out on facebook first

---
+++ Trigger-Protect/app/facebook_filter.js
@@ -0,0 +1,31 @@
+(function() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var newNodes = mutation.addedNodes;
+            if (newNodes !== null) {
+
+                var nodes = document.querySelectorAll('.userContentWrapper, ._1bar, ._5my2, ._4qjp, ._2kg4');
+                for (var ii = 0, nn = nodes.length; ii < nn; ii++) {
+                    var text = nodes[ii] ? nodes[ii].textContent.toLowerCase() : '';
+
+//TODO adding keywords
+                    if (text && text.indexOf('keyword1') >= 0 && text.indexOf('keyword2') >= 0 && nodes[ii].style.display != 'none') {
+                        nodes[ii].style.display = 'none';
+                        chrome.runtime.sendMessage({
+                            action: "removeTrigger"
+                        });
+                    }
+                }
+
+            }
+        });
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+    });
+
+})();
